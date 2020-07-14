8e080e939ecd39360e7aa79be5f082eb9ac812f4
liam roberts <liamroberts77@googlemail.com>
2020-07-14 15:04:27 +0100

    setup enzyme and created first failing test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Counter Testing', () => {
+  test('render the title of counter', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText('Counter app');
+    expect(linkElement).toBeInTheDocument();
+  })
+
 });
+
+
