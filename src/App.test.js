105b6545570191495578b46b60d805394bf11a60
liam roberts <liamroberts77@googlemail.com>
2020-07-14 15:11:18 +0100

    configured enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
 describe('Counter Testing', () => {
   test('render the title of counter', () => {
     const { getByText } = render(<App />);
