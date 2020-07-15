f91c28238657404abd341cbd6a0a2de5b4487f02
liam roberts <liamroberts77@googlemail.com>
2020-07-15 20:41:57 +0100

    ae

--- src/App.test.js
+++ src/App.test.js
@@ -21,6 +21,10 @@ describe("Counter Testing", () => {
     expect(wrapper.find("#increment-btn").text()).toBe("Increment");
   })
 
+  test("render a button with text of decrement", () => {
+    expect(wrapper.find("#decrement-btn").text()).toBe("Decrement");
+  })
+
   test("render the initial value of state in a div", () => {
     expect(wrapper.find("#counter-value").text()).toBe("0");
   })
@@ -29,5 +33,7 @@ describe("Counter Testing", () => {
     wrapper.find("#increment-btn").simulate("click")
     expect(wrapper.find("#counter-value").text()).toBe("1")
   })
+
+
 
 });
