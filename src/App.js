30e2b24a6f792d685e47b7a42a3ac50a53d8dfe5
liam roberts <liamroberts77@googlemail.com>
2020-07-16 11:02:01 +0100

    refactored counter into separate component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import Counter from './Counter.js';
 import './App.css';
 
 function App() {
-  const [counter, setcounter] = React.useState(0);
   return (
     <div className="App">
-      <h1>Counter app</h1>
-      <div id="counter-value">{counter}</div>
-      <button id="increment-btn" onClick={() => setcounter( counter + 1 )}>Increment</button>
-      <button id="decrement-btn" onClick={() => setcounter( counter - 1 )}>Decrement</button>
+      <Counter/>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
-
 import { shallow } from 'enzyme';
-
 
 describe("Counter Testing", () => {
 
