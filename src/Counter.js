5008bce10c467852b11d231141dc01d9cbbb1be2
liam roberts <liamroberts77@googlemail.com>
2020-07-16 17:00:32 +0100

    added reset logic to reset button and changed counter from const to var, passed test

---
+++ src/Counter.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Counter() {
+  var [counter, setcounter] = React.useState(0);
+  return(
+    <div>
+      <h1>Counter app</h1>
+      <div id="counter-value">{counter}</div>
+      <button id="increment-btn" onClick={() => setcounter( counter + 1 )}>Increment</button>
+      <button id="decrement-btn" onClick={() => setcounter( counter - 1 )}>Decrement</button>
+      <button id="reset-btn" onClick={() => setcounter( counter = 0 )}>Reset</button>
+    </div>
+  )
+}
