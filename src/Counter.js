b6fa3f76c7b936b3c9b0772c1079e6086eb1c9c0
liam roberts <liamroberts77@googlemail.com>
2020-09-11 20:03:34 +0100

    changed labels on buttons in counter.js

--- src/Counter.js
+++ src/Counter.js
@@ -4,10 +4,10 @@ export default function Counter() {
   var [counter, setcounter] = React.useState(0);
   return(
     <div>
-      <h1>Counter app</h1>
+      <h1>BBall Score</h1>
       <div id="counter-value">{counter}</div>
-      <button id="increment-btn" onClick={() => setcounter( counter + 1 )}>Increment</button>
-      <button id="decrement-btn" onClick={() => setcounter( counter - 1 )}>Decrement</button>
+      <button id="increment-btn" onClick={() => setcounter( counter + 1 )}>+1</button>
+      <button id="decrement-btn" onClick={() => setcounter( counter - 1 )}>-1</button>
       <button id="reset-btn" onClick={() => setcounter( counter = 0 )}>Reset</button>
       <button id="square-btn" onClick={() => setcounter( counter * counter )}>Square</button>
     </div>
