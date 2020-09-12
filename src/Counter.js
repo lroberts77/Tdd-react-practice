6dceab4c60c0ac685b22bf595bdc5a202cf54633
liam roberts <liamroberts77@googlemail.com>
2020-09-12 15:54:30 +0100

    changed first counter to countera to signify team a

--- src/Counter.js
+++ src/Counter.js
@@ -1,15 +1,15 @@
 import React from "react"
 
 export default function Counter() {
-  var [counter, setcounter] = React.useState(0);
+  var [countera, setcountera] = React.useState(0);
   var [counterb, setcounterb] = React.useState(0);
   return(
     <div>
       <h1>Score</h1>
-      <div id="counter-value">{counter}</div>
-      <button id="increment-btn" onClick={() => setcounter( counter + 1 )}>+1</button>
-      <button id="decrement-btn" onClick={() => setcounter( counter - 1 )}>-1</button>
-      <button id="reset-btn" onClick={() => setcounter( counter = 0 )}>Reset</button>
+      <div id="counter-value">{countera}</div>
+      <button id="increment-btn" onClick={() => setcountera( countera + 1 )}>+1</button>
+      <button id="decrement-btn" onClick={() => setcountera( countera - 1 )}>-1</button>
+      <button id="reset-btn" onClick={() => setcountera( countera = 0 )}>Reset</button>
       {/* <button id="square-btn" onClick={() => setcounter( counter * counter )}>Square</button> */}
       <div id="counter-valueb">{counterb}</div>
       <button id="increment-btn" onClick={() => setcounterb( counterb + 1 )}>+1</button>
