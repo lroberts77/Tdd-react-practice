36cf1efcfbdc67a59fee97a4eb08ce62dab2fb5f
liam roberts <liamroberts77@googlemail.com>
2020-09-12 15:46:11 +0100

    added second state for counterb (second team)

--- src/Counter.js
+++ src/Counter.js
@@ -2,9 +2,10 @@ import React from "react"
 
 export default function Counter() {
   var [counter, setcounter] = React.useState(0);
+  var [counterb, setcounterb] = React.useState(0);
   return(
     <div>
-      <h1>BBall Score</h1>
+      <h1>Score</h1>
       <div id="counter-value">{counter}</div>
       <button id="increment-btn" onClick={() => setcounter( counter + 1 )}>+1</button>
       <button id="decrement-btn" onClick={() => setcounter( counter - 1 )}>-1</button>
