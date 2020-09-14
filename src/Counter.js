45162583567efcb6f01081f786930fb52c89201f
liam roberts <liamroberts77@googlemail.com>
2020-09-14 22:36:51 +0100

    changed the button id of teams to a and b for clarity

--- src/Counter.js
+++ src/Counter.js
@@ -7,13 +7,13 @@ export default function Counter() {
     <div>
       <h1>Score</h1>
       <div id="counter-value">{countera}</div>
-      <button id="increment-btn" onClick={() => setcountera( countera + 1 )}>+1</button>
-      <button id="decrement-btn" onClick={() => setcountera( countera - 1 )}>-1</button>
+      <button id="increment-btna" onClick={() => setcountera( countera + 1 )}>+1</button>
+      <button id="decrement-btna" onClick={() => setcountera( countera - 1 )}>-1</button>
       <button id="reset-btn" onClick={() => setcountera( countera = 0 )}>Reset</button>
       {/* <button id="square-btn" onClick={() => setcounter( counter * counter )}>Square</button> */}
       <div id="counter-valueb">{counterb}</div>
-      <button id="increment-btn" onClick={() => setcounterb( counterb + 1 )}>+1</button>
-      <button id="decrement-btn" onClick={() => setcounterb( counterb - 1 )}>-1</button>
+      <button id="increment-btnb" onClick={() => setcounterb( counterb + 1 )}>+1</button>
+      <button id="decrement-btnb" onClick={() => setcounterb( counterb - 1 )}>-1</button>
       <button id="reset-btn" onClick={() => setcounterb( counterb = 0 )}>Reset</button>
     </div>
   )
