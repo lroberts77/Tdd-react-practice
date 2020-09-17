4d0f34b98bb0d029d0b05617b299fc670faa515a
liam roberts <liamroberts77@googlemail.com>
2020-09-17 17:26:18 +0100

    added logic to +3 button and passed test to render the click event of increment3-btn and increment counter value

--- src/Counter.js
+++ src/Counter.js
@@ -8,13 +8,13 @@ export default function Counter() {
       <h1>Score</h1>
       <div id="counter-valuea">{countera}</div>
       <button id="increment-btna" onClick={() => setcountera( countera + 1 )}>+1</button>
-      <button id="increment3-btna">+3</button>
+      <button id="increment3-btna" onClick={() => setcountera( countera + 3 )}>+3</button>
       <button id="decrement-btna" onClick={() => setcountera( countera - 1 )}>-1</button>
       <button id="reset-btna" onClick={() => setcountera( countera = 0 )}>Reset</button>
       {/* <button id="square-btn" onClick={() => setcounter( counter * counter )}>Square</button> */}
       <div id="counter-valueb">{counterb}</div>
       <button id="increment-btnb" onClick={() => setcounterb( counterb + 1 )}>+1</button>
-      <button id="increment3-btnb">+3</button>
+      <button id="increment3-btnb" onClick={() => setcounterb( counterb + 3 )}>+3</button>
       <button id="decrement-btnb" onClick={() => setcounterb( counterb - 1 )}>-1</button>
       <button id="reset-btnb" onClick={() => setcounterb( counterb = 0 )}>Reset</button>
     </div>
