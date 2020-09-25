7a503ae6f4b2b245a75f7bea4d8b4bb695e9c557
liam roberts <liamroberts77@googlemail.com>
2020-09-25 11:49:24 +0100

    refactored mounted reset-btnb test into mounted click events test

--- src/App.test.js
+++ src/App.test.js
@@ -158,14 +158,8 @@ describe("counter test mount", () => {
       expect(wrapper.find("#counter-valueb").text()).toBe("4");
       wrapper.find("#decrement-btnb").simulate("click");
       expect(wrapper.find("#counter-valueb").text()).toBe("3");
-    })
-
-    test("render the click event of reset-btna to reset counter value", () => {
-      wrapper.find("#increment3-btnb").simulate("click");
-      expect(wrapper.find("#counter-valueb").text()).toBe("3");
       wrapper.find("#reset-btnb").simulate("click");
       expect(wrapper.find("#counter-valueb").text()).toBe("0");
-
     })
 
 })
