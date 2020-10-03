7c83f1787b6b8b50e2be8978a9d7f87206409578
liam roberts <liamroberts77@googlemail.com>
2020-10-03 12:39:32 +0100

    added button with text -1 min and passed test

--- src/Counter.js
+++ src/Counter.js
@@ -56,6 +56,7 @@ export default function Counter() {
       <button id="timerPlus10s" onClick={() => setseconds( seconds => seconds + 10 )}>+10s</button>
       <button id="timerPlus1min" onClick={() => setminutes( minutes => minutes + 1 )}>+1 min</button>
       <button id="timerMinus10s" onClick={() => setseconds( seconds => seconds - 10 )}>-10s</button>
+      <button id="timerMinus1min">-1 min</button>
       <div className="timer">
         <span id="min" className="minutes">{minutes}</span>:<span id="sec" className="seconds">{seconds}</span>
       </div>
