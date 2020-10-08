0bede03dd5a886e0fc66b6ee2fa7de5b15a622c4
liam roberts <liamroberts77@googlemail.com>
2020-10-08 13:06:03 +0100

    added team a score adjustment buttons to team a grid column

--- src/Counter.js
+++ src/Counter.js
@@ -116,28 +116,33 @@ export default function Counter() {
         <h1 id="score">Score</h1>
       </Grid>
       <Grid>
-        <Grid>
-        <div id="team-a">TEAM A</div>
+        <Grid item xs={3} 
+            container
+            direction="column"
+            justify="center"
+            alignItems="center">
+          <div id="team-a">TEAM A</div>
+          <button id="increment-btna" onClick={() => setcountera( countera + 1 )}>+1</button>
+          <button id="increment3-btna" onClick={() => setcountera( countera + 3 )}>+3</button>
+          <button id="decrement-btna" onClick={() => setcountera( countera - 1 )}>-1</button>
+          <button id="reset-btna" onClick={() => setcountera( countera = 0 )}>Reset</button>
         </Grid>
 
-        <div id="counter-valuea">{countera}</div>
-        <button id="increment-btna" onClick={() => setcountera( countera + 1 )}>+1</button>
-        <button id="increment3-btna" onClick={() => setcountera( countera + 3 )}>+3</button>
-        <button id="decrement-btna" onClick={() => setcountera( countera - 1 )}>-1</button>
-        <button id="reset-btna" onClick={() => setcountera( countera = 0 )}>Reset</button>
-        <div id="team-b">TEAM B</div>
-        <div id="counter-valueb">{counterb}</div>
-        <button id="increment-btnb" onClick={() => setcounterb( counterb + 1 )}>+1</button>
-        <button id="increment3-btnb" onClick={() => setcounterb( counterb + 3 )}>+3</button>
-        <button id="decrement-btnb" onClick={() => setcounterb( counterb - 1 )}>-1</button>
-        <button id="reset-btnb" onClick={() => setcounterb( counterb = 0 )}>Reset</button>
-      </Grid>
-        <div id="fouls-b">{foulsb}</div>
-        <button id="foulsplus1-btnb" onClick={() => setfoulsb( foulsb + 1)}>+1</button>
-        <button id="fouls-btnb" onClick={() => setfoulsb( foulsb = 0 )}>Reset</button>
-        <div id="fouls-a">{foulsa}</div>
-        <button id="foulsplus1-btna" onClick={() => setfoulsa( foulsa + 1)}>+1</button>
-        <button id="fouls-btna" onClick={() => setfoulsa( foulsa = 0 )}>Reset</button>
+          <div id="counter-valuea">{countera}</div>
+
+          <div id="team-b">TEAM B</div>
+          <div id="counter-valueb">{counterb}</div>
+          <button id="increment-btnb" onClick={() => setcounterb( counterb + 1 )}>+1</button>
+          <button id="increment3-btnb" onClick={() => setcounterb( counterb + 3 )}>+3</button>
+          <button id="decrement-btnb" onClick={() => setcounterb( counterb - 1 )}>-1</button>
+          <button id="reset-btnb" onClick={() => setcounterb( counterb = 0 )}>Reset</button>
+        </Grid>
+          <div id="fouls-b">{foulsb}</div>
+          <button id="foulsplus1-btnb" onClick={() => setfoulsb( foulsb + 1)}>+1</button>
+          <button id="fouls-btnb" onClick={() => setfoulsb( foulsb = 0 )}>Reset</button>
+          <div id="fouls-a">{foulsa}</div>
+          <button id="foulsplus1-btna" onClick={() => setfoulsa( foulsa + 1)}>+1</button>
+          <button id="fouls-btna" onClick={() => setfoulsa( foulsa = 0 )}>Reset</button>
     </div>
     
   )
