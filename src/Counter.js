db856767a074c1607639804cb9d6da37183b50fe
liam roberts <liamroberts77@googlemail.com>
2020-10-08 12:17:59 +0100

    placed timer in the middle of the page

--- src/Counter.js
+++ src/Counter.js
@@ -74,14 +74,6 @@ export default function Counter() {
           <button id="timerPlus10s" onClick={() => setseconds( seconds => seconds + 10 )}>+10s</button>
           <button id="timerPlus1min" onClick={() => setminutes( minutes => minutes + 1 )}>+1 min</button>
         </Grid>
-        <Grid item xs={3} 
-          container
-          direction="column"
-          justify="center"
-          alignItems="center">
-        <button id="timerMinus10s" onClick={() => setseconds( seconds => seconds - 10 )}>-10s</button>
-        <button id="timerMinus1min" onClick={() => setminutes( minutes => minutes - 1 )}>-1 min</button>
-        </Grid>
         <Grid item xs={6} 
           container
           direction="column"
@@ -90,6 +82,14 @@ export default function Counter() {
         <div className="timer">
           <span id="min" className="minutes">{minutes}</span>:<span id="sec" className="seconds">{seconds}</span>
         </div>
+        </Grid>
+        <Grid item xs={3} 
+          container
+          direction="column"
+          justify="center"
+          alignItems="center">
+        <button id="timerMinus10s" onClick={() => setseconds( seconds => seconds - 10 )}>-10s</button>
+        <button id="timerMinus1min" onClick={() => setminutes( minutes => minutes - 1 )}>-1 min</button>
         </Grid>
         <div className="buttons">
           {isRunning
