05380380ac29675f8f2ef29afda05852158acb3b
liam roberts <liamroberts77@googlemail.com>
2020-10-08 12:59:46 +0100

    added grid for the next row and grid for the first column, added team A to forst column

--- src/Counter.js
+++ src/Counter.js
@@ -110,26 +110,33 @@ export default function Counter() {
         <button id="timerMinus10s" onClick={() => setseconds( seconds => seconds - 10 )}>-10s</button>
         <button id="timerMinus1min" onClick={() => setminutes( minutes => minutes - 1 )}>-1 min</button>
         </Grid>
-        </Grid>
+      </Grid>
+      <Grid item xs={12}>
         <h1 id="score">Score</h1>
+      </Grid>
+      <Grid>
+        <Grid>
         <div id="team-a">TEAM A</div>
+        </Grid>
+
         <div id="counter-valuea">{countera}</div>
         <button id="increment-btna" onClick={() => setcountera( countera + 1 )}>+1</button>
         <button id="increment3-btna" onClick={() => setcountera( countera + 3 )}>+3</button>
         <button id="decrement-btna" onClick={() => setcountera( countera - 1 )}>-1</button>
         <button id="reset-btna" onClick={() => setcountera( countera = 0 )}>Reset</button>
-        <div id="fouls-a">{foulsa}</div>
-        <button id="foulsplus1-btna" onClick={() => setfoulsa( foulsa + 1)}>+1</button>
-        <button id="fouls-btna" onClick={() => setfoulsa( foulsa = 0 )}>Reset</button>
         <div id="team-b">TEAM B</div>
         <div id="counter-valueb">{counterb}</div>
         <button id="increment-btnb" onClick={() => setcounterb( counterb + 1 )}>+1</button>
         <button id="increment3-btnb" onClick={() => setcounterb( counterb + 3 )}>+3</button>
         <button id="decrement-btnb" onClick={() => setcounterb( counterb - 1 )}>-1</button>
         <button id="reset-btnb" onClick={() => setcounterb( counterb = 0 )}>Reset</button>
+      </Grid>
         <div id="fouls-b">{foulsb}</div>
         <button id="foulsplus1-btnb" onClick={() => setfoulsb( foulsb + 1)}>+1</button>
         <button id="fouls-btnb" onClick={() => setfoulsb( foulsb = 0 )}>Reset</button>
+        <div id="fouls-a">{foulsa}</div>
+        <button id="foulsplus1-btna" onClick={() => setfoulsa( foulsa + 1)}>+1</button>
+        <button id="fouls-btna" onClick={() => setfoulsa( foulsa = 0 )}>Reset</button>
 
     </div>
     
