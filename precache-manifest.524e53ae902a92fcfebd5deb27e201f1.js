fe5bdb6839c9e8159674df49f4ff44ec4aadab09
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-01 14:21:15 +0000

    Deploy lroberts77/Tdd-react-practice to github.com/lroberts77/Tdd-react-practice.git:gh-pages

---
+++ precache-manifest.524e53ae902a92fcfebd5deb27e201f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d991e476bbddb8688218f3323ec7e7af",
+    "url": "/Tdd-react-practice/index.html"
+  },
+  {
+    "revision": "410be5b57e0d007ec2ae",
+    "url": "/Tdd-react-practice/static/css/main.770a56bc.chunk.css"
+  },
+  {
+    "revision": "b7072b4b82e065ae3010",
+    "url": "/Tdd-react-practice/static/js/2.5fa5a0f6.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/Tdd-react-practice/static/js/2.5fa5a0f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "410be5b57e0d007ec2ae",
+    "url": "/Tdd-react-practice/static/js/main.c51d60f6.chunk.js"
+  },
+  {
+    "revision": "6ef55074dbe9046cb4d6",
+    "url": "/Tdd-react-practice/static/js/runtime-main.b47e43dd.js"
+  },
+  {
+    "revision": "05dbb6b91ee51c1d1df8e09a8cc15b43",
+    "url": "/Tdd-react-practice/static/media/bg2.05dbb6b9.jpg"
+  }
+]);
